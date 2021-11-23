9b1a78f82fdc2c22d7fbe697f5da24d712bbfd3a
imcastbound <d.b.sanchez2000@gmail.com>
2021-11-23 14:35:03 +0800

    users added

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const userData = require('./metroevents_users.json')
+
+const port = process.env.PORT || 3000
+
+app.get('/', (req ,res)=>{
+    res.send("Metro Events API")
+})
+
+app.get('/metroevents', (req, res)=>{
+    res.send(userData)
+})
+
+app.listen(port, ()=> {
+    console.log('App is listening to port ${port}')
+})
